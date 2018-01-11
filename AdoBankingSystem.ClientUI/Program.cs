ac136d669ecf073b96d1b8c0e5313f66ad15b224
Айбек <@DESKTOP-UKQVP6O>
2018-01-11 21:41:58 +0300

    Fixed resources

--- AdoBankingSystem.ClientUI/Program.cs
+++ AdoBankingSystem.ClientUI/Program.cs
@@ -17,7 +17,7 @@ namespace AdoBankingSystem.ClientUI
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new RegistrationWindow());
+            Application.Run(new TransactionForm());
         }
     }
 }
